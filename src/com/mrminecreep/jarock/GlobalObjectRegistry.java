cfa2b2ae031191b2d0bcb29d12d681f42d2b1c12
Itzyo <progamerandie64@gmail.com>
2020-04-06 22:07:27 +0200

    Update javadoc

--- src/com/mrminecreep/jarock/GlobalObjectRegistry.java
+++ src/com/mrminecreep/jarock/GlobalObjectRegistry.java
@@ -2,18 +2,42 @@ package com.mrminecreep.jarock;
 
 import java.util.HashMap;
 
+/**
+ * Registry for referencing globally used objects.
+ * 
+ * @author MrMinecreep
+ *
+ */
 public class GlobalObjectRegistry {
 	
+	/**
+	 * Container for global Objects.
+	 */
 	private static HashMap<Integer, Object> objs = new HashMap<Integer, Object>();
 	
+	/**
+	 * Register a new global object with an <b>unique</b> id.
+	 * 
+	 * @param obj object to register.
+	 * @param id id to link with the object.
+	 */
 	public static void RegisterObject(Object obj, int id) {
 		objs.put(id, obj);
 	}
 	
+	/**
+	 * Get the Object linked with an id.
+	 * @param id the id the object was linked with.
+	 * @return the requested object.
+	 */
 	public static Object get(int id) {
 		return objs.get(id);
 	}
 	
+	/**
+	 * Unregisters a global object.
+	 * @param id the id of the object to unregister.
+	 */
 	public static void unregister(int id) {
 		objs.remove(id);
 	}
