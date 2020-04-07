5fc6c0a0a0392b10965069cb963c1b915b59b1fe
Itzyo <progamerandie64@gmail.com>
2020-04-07 23:03:48 +0200

    Package refactor

--- src/com/mrminecreep/jarock/networking/Testhandler.java
+++ src/com/mrminecreep/jarock/networking/Testhandler.java
@@ -1,4 +1,4 @@
-package com.networking;
+package com.mrminecreep.jarock.networking;
 import io.netty.buffer.ByteBuf;
 
 import io.netty.channel.ChannelHandlerContext;
--- src/com/mrminecreep/jarock/networking/Testmain.java
+++ src/com/mrminecreep/jarock/networking/Testmain.java
@@ -1,4 +1,4 @@
-package com.networking;
+package com.mrminecreep.jarock.networking;
     
 import io.netty.bootstrap.ServerBootstrap;
 
